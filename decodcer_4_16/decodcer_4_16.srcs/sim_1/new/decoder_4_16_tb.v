5533a0c4b368ccc097b27534624eeed2b9847c29
Enoch Wong <enoch3d14@gmail.com>
2025-07-26 15:23:52 -0700

    Added 4 to 16 decoder script

---
+++ decodcer_4_16/decodcer_4_16.srcs/sim_1/new/decoder_4_16_tb.v
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+
+module decoder_4_16_tb();
+    reg [4:0] a;
+    reg enable;
+    wire [15:0] d;
+endmodule 
--- decodcer_4_16/decodcer_4_16.srcs/sources_1/new/decoder_4_16.v
+++ decodcer_4_16/decodcer_4_16.srcs/sources_1/new/decoder_4_16.v
@@ -1,2 +1,24 @@
 `timescale 1ns / 1ps
 
+module decoder_4_16(
+    input [3:0] a,
+    input enable,
+    output [15:0] y
+    );
+    
+    wire en1 = ~a[3] & enable;
+    wire en2 = a[3] & enable;
+    
+    decoder_3_8 DECODER1(
+        .a(a[2:0]),
+        .enable(en1),
+        .y(y[7:0])
+    );
+    
+    decoder_3_8 DECODER2(
+        .a(a[2:0]),
+        .enable(en2),
+        .y(y[15:8])
+    );
+    
+endmodule
